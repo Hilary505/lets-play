d86551f6b68ab474910a27f3753c6027606f101e
Hilary505 <omondihilary19@gmail.com>
2025-12-07 09:11:54 +0300

    add  audit to mongodb database

--- src/main/java/com/letsplay/LetsPlayApplication.java
+++ src/main/java/com/letsplay/LetsPlayApplication.java
@@ -2,8 +2,10 @@ package com.letsplay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class LetsPlayApplication {
 
     public static void main(String[] args) {
