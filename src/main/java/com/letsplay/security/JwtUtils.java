f977ac47277649a83b68530c4acc9e4bbdb68461
Hilary505 <omondihilary19@gmail.com>
2025-12-10 23:13:44 +0300

    add jwtUtils file

---
+++ src/main/java/com/letsplay/security/JwtUtils.java
@@ -0,0 +1,46 @@
+package com.letsplay.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+	private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+	private final long expiration = 86400000; // 24 hours
+
+	public String generateToken(String username) {
+		return Jwts.builder()
+				.setSubject(username)
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() + expiration))
+				.signWith(key)
+				.compact();
+	}
+
+	public String getUsernameFromToken(String token) {
+		try {
+			return Jwts.parserBuilder()
+					.setSigningKey(key)
+					.build()
+					.parseClaimsJws(token)
+					.getBody()
+					.getSubject();
+		} catch (JwtException e) {
+			return null;
+		}
+	}
+
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+			return true;
+		} catch (JwtException e) {
+			return false;
+		}
+	}
+}
